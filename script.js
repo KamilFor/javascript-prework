30fe21e0a61d729f384402d0da7df69d77983bb8
KamilFor <67376189+KamilFor@users.noreply.github.com>
2020-06-29 21:20:48 +0200

    Add task 5.3

---
+++ script.js
@@ -0,0 +1,16 @@
+function printMessage(msg) {
+  let div = document.createElement("div");
+  div.innerHTML = msg;
+  document.getElementById("messages").appendChild(div);
+}
+
+function clearMessages() {
+  document.getElementById("messages").innerHTML = "";
+}
+
+let computerMove = `kamień`;
+let playerMove = "papier";
+
+printMessage("Zagrałem " + computerMove + "! Jeśli Twój ruch to papier, to wygrywasz!");
+
+printMessage("Zagrałem " + playerMove + "! Jeśli Twój ruch to papier, to wygrywasz!");
