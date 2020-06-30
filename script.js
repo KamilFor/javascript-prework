2b908e607ba09a9827a07ef2eea2f6e5adf9f55c
KamilFor <67376189+KamilFor@users.noreply.github.com>
2020-06-30 02:11:58 +0200

    Zadanie 5.6
    Nie chciała funkcja addEventListener czytać sama z siebie liczby :  np. nozyce.addEventListener("click", playGame(3));
    Nie wiem czemu, ale nie chciała działać i wykonywała funkcję tylko przy odświeżeniu strony, więc dodałem dodatkową funkcje first second lub third, by ominąć ten problem.

--- script.js
+++ script.js
@@ -8,51 +8,68 @@ function clearMessages() {
   document.getElementById("messages").innerHTML = "";
 }
 
-let computerMove = Math.floor(Math.random() * 3 + 1);
-let playerMove = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.");
-let wining;
-console.log("Wylosowana liczba to: " + computerMove);
-console.log("Gracz wpisał: " + playerMove);
+let papier = document.getElementById("papier");
+papier.addEventListener("click", first);
+function first() {
+  playGame(2);
+}
+let kamien = document.getElementById("kamien");
+kamien.addEventListener("click", second);
+function second() {
+  playGame(1);
+}
+let nozyce = document.getElementById("nozyce");
+nozyce.addEventListener("click", third);
+function third() {
+  playGame(3);
+}
 
-function displayResult(computerMove, playerMove, wining) {
-  if (computerMove == playerMove) {
-    wining = "remis";
-  } else if (computerMove == 1) {
-    if (computerMove < playerMove) {
-      wining = "przegrywasz";
+function playGame(playerMove) {
+  let computerMove = Math.floor(Math.random() * 3 + 1);
+  let wining;
+  console.log("Wylosowana liczba to: " + computerMove);
+  console.log("Gracz wpisał: " + playerMove);
+
+  function displayResult(computerMove, playerMove, wining) {
+    if (computerMove == playerMove) {
+      wining = "remis";
+    } else if (computerMove == 1) {
+      if (computerMove < playerMove) {
+        wining = "przegrywasz";
+      }
+      if (playerMove == 3) {
+        wining = "wygrywasz";
+      }
+    } else if (computerMove == 2) {
+      if (computerMove < playerMove) {
+        wining = "przegrywasz";
+      }
+      if (playerMove == 1) {
+        wining = "wygrywasz";
+      }
+    } else if (computerMove == 3) {
+      if (computerMove > playerMove) {
+        wining = "przegrywasz";
+      }
+      if (playerMove == 2) {
+        wining = "wygrywasz";
+      }
     }
-    if (playerMove == 3) {
-      wining = "wygrywasz";
-    }
-  } else if (computerMove == 2) {
-    if (computerMove < playerMove) {
-      wining = "przegrywasz";
-    }
-    if (playerMove == 1) {
-      wining = "wygrywasz";
-    }
-  } else if (computerMove == 3) {
-    if (computerMove > playerMove) {
-      wining = "przegrywasz";
-    }
-    if (playerMove == 2) {
-      wining = "wygrywasz";
+    return wining;
+  }
+  function getMoveName(name) {
+    if (name == 1) {
+      name = "kamień";
+      return name;
+    } else if (name == 2) {
+      name = "papier";
+      return name;
+    } else if (name == 3) {
+      name = "nożyczki";
+      return name;
     }
   }
-  return wining;
+  printMessage("Mój ruch to: " + getMoveName(computerMove));
+  printMessage("Twój ruch to: " + getMoveName(playerMove));
+  printMessage("Twój ruch to: " + displayResult(computerMove, playerMove, wining));
 }
-function getMoveName(name) {
-  if (name == 1) {
-    name = "kamień";
-    return name;
-  } else if (name == 2) {
-    name = "papier";
-    return name;
-  } else if (name == 3) {
-    name = "nożyczki";
-    return name;
-  }
-}
-printMessage("Mój ruch to: " + getMoveName(computerMove));
-printMessage("Twój ruch to: " + getMoveName(playerMove));
-printMessage("Twój ruch to: " + displayResult(computerMove, playerMove, wining));
